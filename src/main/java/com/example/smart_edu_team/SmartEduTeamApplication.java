0060ab32775d0b79fee4ef3c400147c03f6cc9cf
kmgyu <lidersy961@gmail.com>
2024-06-01 20:04:55 +0900

    project init

---
+++ src/main/java/com/example/smart_edu_team/SmartEduTeamApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_edu_team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEduTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEduTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smart_edu_team/SmartEduTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_edu_team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEduTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
