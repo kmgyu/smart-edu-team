e3b5176cde13f0df255b984e92529ab26cfb3a87
kmgyu <lidersy961@gmail.com>
2024-06-09 21:52:31 +0900

    [fix] delete 기능 계정 확인절차 추가 - 본인 계정이 아님에도 삭제가능한 문제 수정 - post element 수정

--- src/main/java/com/example/smart_edu_team/post/Post.java
+++ src/main/java/com/example/smart_edu_team/post/Post.java
@@ -1,5 +1,6 @@
 package com.example.smart_edu_team.post;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
@@ -16,9 +17,14 @@ import java.util.UUID;
 public class Post {
     @Id
     private String id;
+    @NotEmpty(message = "필수항목입니다.")
     private String title;
+    @NotEmpty(message = "필수항목입니다.")
     private String content;
+    @NotEmpty(message = "필수항목입니다.")
     private String author;
+    @NotEmpty(message = "시간을 알 수 없습니다.")
     private LocalDateTime posted_time;
+    @NotEmpty(message = "시간을 알 수 없습니다.")
     private LocalDateTime edited_time;
 }
